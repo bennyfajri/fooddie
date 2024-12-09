03c5157ec792ef32fe2a6ac00f4f71d254e341c3
Muhammad Beni Fajri <m.b.fajrii@gmail.com>
2024-12-09 10:43:09 +0700

    initial-commit

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import "./Footer.css";
+import { assets } from "../../assets/frontend_assets/assets";
+
+const Footer = () => {
+  return (
+    <div className="footer" id="footer">
+      <div className="footer-content">
+        <div className="footer-content-left">
+          <img src={assets.logo} alt="" />
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.{" "}
+          </p>
+          <div className="footer-social-icons">
+            <img src={assets.facebook_icon} alt="" />
+            <img src={assets.twitter_icon} alt="" />
+            <img src={assets.linkedin_icon} alt="" />
+          </div>
+        </div>
+        <div className="footer-content-center">
+          <h2>COMPANY</h2>
+          <ul>
+            <li>Home</li>
+            <li>About Us</li>
+            <li>Delivery</li>
+            <li>Privacy Policy</li>
+          </ul>
+        </div>
+        <div className="footer-content-right">
+          <h2>GET IN TOUCH</h2>
+          <ul>
+            <li>+628 1234 5678</li>
+            <li>m.b.fajrii@gmail.com</li>
+          </ul>
+        </div>
+      </div>
+      <hr />
+      <p className="footer-copyright">Copyright © 2024. All rights reserved</p>
+    </div>
+  );
+};
+
+export default Footer;
